bc32f94b3303bf93cf7c38d0be3438687d7364bd
akriti <akritikohli05@gmail.com>
2025-03-06 14:33:26 +0530

    url fixed

--- client/src/utils/constants.jsx
+++ client/src/utils/constants.jsx
@@ -1,1 +1,1 @@
-export const base_url = location.hostname === "localhost"?"http://localhost:3000" : "https://essence-46no.onrender.com";
+export const base_url = location.hostname === "localhost"?"http://localhost:3000" : "https://watertankmanagement.onrender.com";
